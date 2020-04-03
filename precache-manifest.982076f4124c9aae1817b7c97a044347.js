fd4fcc5d6d0030c0c3dea0c09571a5be72d0679c
romainpajda <48944361+romainpajda@users.noreply.github.com>
2020-04-03 11:52:14 +0200

    Updates

---
+++ precache-manifest.982076f4124c9aae1817b7c97a044347.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dabad34f166da8d727f304913bf0b19",
+    "url": "/commando-covid-deploye/index.html"
+  },
+  {
+    "revision": "b419809dece949fffe00",
+    "url": "/commando-covid-deploye/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "33b4b69b9a6adac3d31c",
+    "url": "/commando-covid-deploye/static/js/2.d44a4c51.chunk.js"
+  },
+  {
+    "revision": "b82dc1caedd6da17de2528470bfc0c08",
+    "url": "/commando-covid-deploye/static/js/2.d44a4c51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b419809dece949fffe00",
+    "url": "/commando-covid-deploye/static/js/main.81e6f2cb.chunk.js"
+  },
+  {
+    "revision": "16932832b60968ed054b",
+    "url": "/commando-covid-deploye/static/js/runtime-main.e31aa163.js"
+  }
+]);
